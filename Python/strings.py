1abd2058b8991d193fe2388f095ee17693865ecd
Diego Lessa <dgclessa@gmail.com>
2025-06-25 14:57:59 -0300

    manipulacao de string v_2

--- Python/strings.py
+++ Python/strings.py
@@ -1,21 +1,21 @@
-nome = 'Diego '
-sobrenome = 'Coelho Lessa'
-letra = nome[2]
-print(letra)
-print(len(nome))
-print(nome + sobrenome)
+# nome = 'Diego '
+# sobrenome = 'Coelho Lessa'
+# letra = nome[2]
+# print(letra)
+# print(len(nome))
+# print(nome + sobrenome)
 
-frase = 'Vamos aprender Python hoje'
-palavras = frase.split()
-print(palavras[1])
+# frase = 'Vamos aprender Python hoje'
+# palavras = frase.split()
+# print(palavras[1])
 
-for palavra in palavras:
-    print(palavra)
+# for palavra in palavras:
+#     print(palavra)
 
-for letra in frase:
-    print(letra)
+# for letra in frase:
+#     print(letra)
     
-print(frase[0:5],'\n')
+# print(frase[0:5],'\n')
 
 
 email = input('Digite seu email: ')
