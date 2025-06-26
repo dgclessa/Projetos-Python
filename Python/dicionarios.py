347dd82facac32147dab3d8a69d0c812a07d6299
Diego Lessa <dgclessa@gmail.com>
2025-06-26 14:57:25 -0300

    dicionarios

---
+++ Python/dicionarios.py
@@ -0,0 +1,12 @@
+#Dicionarios
+
+elemento = {
+    'Z': 3,
+    'nome':'Lítio',
+    'Grupo': 'Metais Alcalinos',
+    'densidade':0.534
+    
+}
+
+print(f'Elemento: {elemento['nome']}')
+print(f'Densidade: {elemento['densidade']}')
